21acb716e4325b77f6598d7dcc982c548e2d0eb0
Joshua Moore <jmjoshua7465@gmail.com>
2020-05-14 22:08:38 -0600

    Initial Commit

---
+++ SymbolsUploader/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SymbolsUploader
+//
+//  Created by Joshua Moore on 5/14/20.
+//  Copyright © 2020 Joshua Moore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SymbolsUploaderTests/SymbolsUploaderTests.swift
@@ -0,0 +1,34 @@
+//
+//  SymbolsUploaderTests.swift
+//  SymbolsUploaderTests
+//
+//  Created by Joshua Moore on 5/14/20.
+//  Copyright © 2020 Joshua Moore. All rights reserved.
+//
+
+import XCTest
+@testable import SymbolsUploader
+
+class SymbolsUploaderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
